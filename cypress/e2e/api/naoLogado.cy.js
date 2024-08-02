870925be04e425583a5cdd35acca48b391cefa95
Bruno <studybruno000@gmail.com>
2024-08-01 22:52:35 -0300

    Testing API with Cypress (Get method) at conexaoQA website

---
+++ cypress/e2e/api/naoLogado.cy.js
@@ -0,0 +1,80 @@
+describe('API - Profile', () => {
+    context('todos os perfis', () => {
+        it('valida a API de perfis', () => {
+
+            cy.log('Teste de texto')
+            cy.request({
+                method: 'GET',
+                url: '/api/profile',
+            }).then(({ status,duration,body,headers }) => {
+                expect(status).to.eq(200)
+                expect(duration).to.be.lessThan(10000)
+                expect(body[0].status).to.eq('QA Pleno')
+                expect(body[0].company).to.eq( 'Itau')
+                expect(body[0].education[0].degree).to.eq('Tecnico')
+                expect(body[0].education[0].school).to.eq('Etec')
+                expect(body[0].skills[0]).to.eq('Ccypress')
+                expect(body[0].skills).to.have.lengthOf(1)
+                expect(body[0].date).to.not.be.string
+                expect(body[0].date).to.not.be.null
+                expect(body[0].date).to.not.be.NaN
+                //Wrong way:
+                //expect(respostaAPI.headers.x-powered-by).to.eq('Express')
+                //Correct way:
+                expect(headers['x-powered-by']).to.eq('Express')
+            })
+        });
+    });
+
+    context('Specific Profile', () => {
+        it('Select an invalid user', () => {
+            cy.request({
+                method: 'GET',
+                url:'/api/profile/user/1',
+                failOnStatusCode: false
+            }).then(respostaAPI => {
+                expect(respostaAPI.status).to.eq(404)
+                expect(respostaAPI.body.errors[0].msg).to.eq('Perfil não encontrado')
+                expect(respostaAPI.duration).to.be.lessThan(10000)
+                expect(respostaAPI.duration).to.be.greaterThan(1)
+            })
+        })
+        it('validates a validated user', () => {
+            let usuarioId = '66a829478ba90c27884e77b6'
+            cy.request({
+                method: 'GET',
+                url: `/api/profile/user/${usuarioId}`
+            }).then(({ status,body}) => {
+                expect(status).to.eq(200)
+                expect(body.company).to.eq('Itau')
+                expect(body.bio).to.eq('QA pleno')
+                expect(body.education[0].degree).to.eq('Tecnico')
+                expect(body.education[0].fieldofstudy).to.eq('DS')
+                expect(body.experience[0].company).to.eq('WKL solutions')
+                expect(body.experience[0].description).to.eq('QA junior')
+                expect(body.githubusername).to.eq('DevBruno316')
+                expect(body.status).to.eq('QA Pleno')
+            })
+        })
+        it.only('validates a validated user searching in the base', () => {
+            let usuarioId = ''
+            cy.request({
+                method: 'GET',
+                url: '/api/profile'
+            }).then(({ body }) => {
+                usuarioId = body[0].user._id
+
+                cy.request({
+                    method: 'GET',
+                    url:`/api/profile/user/${usuarioId}`
+                }).then(({ status,body }) => {
+                    expect(status).to.eq(200)
+                    expect(body.location).to.eq('Sao paulo')
+                    expect(body.skills[0]).to.eq('Ccypress')
+                })
+            })
+
+        })
+        
+    })
+});
