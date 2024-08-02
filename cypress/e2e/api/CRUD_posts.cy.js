8f4bd9af861f452ca20dfe7427998694977c76e3
Bruno <studybruno000@gmail.com>
2024-08-02 17:21:16 -0300

    Finalizated CRUD testing

--- cypress/e2e/api/CRUD_posts.cy.js
+++ cypress/e2e/api/CRUD_posts.cy.js
@@ -1,19 +1,76 @@
 describe('CRUD - Posts', () => {
 
-    before(() => {
+    let postId = ''
+    let message = 'This post was made from Cypress'
+
+
+    beforeEach(() => {
         
         
+       cy.login(Cypress.env('email'), Cypress.env('password'))
+    })
+
+    it('Create a post', () => {
         cy.request({
             method: 'POST',
-            url: '/api/auth',
+            url: '/api/posts' ,
             body: {
-                email:'testepost@teste.com',
-                password: '1234567890'
+                text: message
             }
+        }).then(({status,body}) => {
+            expect(status).to.eq(201)
+            expect(body.name).to.eq('Teste POST')
+            expect(body.user).to.eq('66ac3d09b7343c204c93cd38')
+            postId = body._id
+        })
+    })
+    it('Read post', () => {
+        
+        cy.request({
+            method: 'GET',
+            url: `/api/posts/${postId}`
+        }).then(({ status,body}) => {
+            expect(status).to.eq(200)
+            expect(body.text).to.eq(message)
+            expect(body.likes).to.have.lengthOf(0)
         })
     })
 
-    it('teste', () => {
-        cy.log('teste')
+    it('Update Post', () => {
+        cy.request({
+            method: 'PUT',
+            url: `/api/posts/like/${postId}`
+        }).then(({status}) => {
+            expect(status).to.eq(200)
+
+            cy.request({
+                method: 'GET',
+                url: `/api/posts/${postId}`
+            }).then(({body}) => {
+                expect(body.likes).to.have.lengthOf(1)
+            })
+        })
     })
+
+    it('Delete Post', () => {
+        cy.request({
+            method: 'DELETE',
+            url:`/api/posts/${postId}`
+
+        }).then(({status, body}) => {
+            expect(status).to.eq(200)
+            expect(body.msg).to.eq('Post removido')
+
+
+            cy.request({
+                method: 'GET',
+                url: `/api/posts/${postId}`,
+                failOnStatusCode: false
+            }).then(({status}) => {
+                expect(status).to.eq(404)
+            })
+        })
+    })
+
+   
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,25 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import Ajv from 'ajv'
+
+
+Cypress.Commands.add('login', (email,password) => {
+    cy.request({
+        method: 'POST',
+        url: '/api/auth',
+        body: {
+                email:email,
+                password: password
+              }
+        })
+    
+})
+
+Cypress.Commands.add('testeContrato', () => {
+
+    // Função que mostra os erros 
+    const getSchemaError = (ajvErrors) => {
+        return cy.wrap(
+            `Campo: ${ajvErrors[0]['instancePath']} is invalid. Error: ${ajvErrors[0]['message']}`
+        )
+    }
+})
+
